2f20942ab3c5d1741489ce41dfcec53ab6371038
Julien Raigneau <julien@zeneffy.fr>
2011-02-12 19:02:14 +0100

    version 0.3 with tooltips

--- watcher.rb
+++ watcher.rb
@@ -6,16 +6,6 @@ require 'appengine-apis/urlfetch'
 DataMapper.setup(:default, "appengine://auto")
 
 # Create your model class
-class Result
-  include DataMapper::Resource
-  property :id,     Serial
-  property :date,   Time
-  property :value,  Float
-  
-  property :test_id,   Integer, :key => true, :min => 1
-  belongs_to :test, :key => true  # defaults to :required => true
-end
-
 class Test
   include DataMapper::Resource
   property :id,     Serial
@@ -23,6 +13,15 @@ class Test
   
   has n, :results
   has n, :archives
+end
+
+class Result
+  include DataMapper::Resource
+  property :id,     Serial
+  property :date,   Time
+  property :value,  Float
+    
+  belongs_to :test, :child_key => [:test_id]  # defaults to :required => true
 end
 
 class Archive
@@ -35,10 +34,10 @@ class Archive
   property :min,    Float
   property :uptime, Float
   property :nbValues, Integer
-  
-  property :test_id,   Integer, :key => true, :min => 1
-  belongs_to :test, :key => true  # defaults to :required => true
+
+  belongs_to :test, :child_key => [:test_id]  # defaults to :required => true
 end
+
 
 # Make sure our template can use <%=h
 helpers do
@@ -63,23 +62,12 @@ get '/' do
     @array[line]["url"] = test.url
     @array[line]["id"] = test.id
     archives = test.archives.all(:type => "day",:date.gte => todayB,:date.lte => todayL,:order => [:date.asc])
-    archives.each do |archive|
-      puts "------- #{archive} #{archive.date}"
+    archives.each do |archive|      
       @array[line][archive.date] = archive
     end
     line = line + 1
   end
-
-  @array[0].values.each do |day|
-    puts "#### #{day}"
-  end
-  (1..@array.count-1).each do |i| 
-    puts "url #{@array[i]["url"]}"
-    @array[0].keys.each do |day|
-      puts "=>=> #{@array[i][day]}"
-    end
-    puts "id #{@array[i]["id"]}"
-  end
+  
   erb :index
 end
 
@@ -106,41 +94,39 @@ get '/:id/delete' do
   redirect '/'
 end
 
-#TODO
-def archives(type, dateB, dateL)
-end
 
-
-#in cron tab
-get '/archives/day' do
-  tests = Test.all
-  today = Time.now - 86400 #yesterday
-  todayB = Time.local(today.year,today.month,today.day) #00:00
-  todayL = Time.local(today.year,today.month,today.day,23,59) #23:59
-  tests.each do |test|
-    results = test.results.all(:date.gte => todayB,:date.lte => todayL)
-    max,min,mean,nb,uptime = 0,0,0,0,0
-    results.each do |result|
-      value = result.value      
-      if value > 0
-        mean = mean + value
-        if value > max
-          max = value
-        end
-        if value < min
-          min = value
-        end
-        uptime = uptime + 1
+def archivesDay(value,date,test)
+  todayB = Time.local(date.year,date.month,date.day) #00:00
+  archive = test.archives.first(:date => todayB)
+  if archive != nil
+    max,min,mean,uptime,nbValues = archive.max,archive.min,archive.mean,archive.uptime,archive.nbValues
+    if archive.uptime == 0 and nbValues > 0 #first measures were KO
+      #no change but nb of values
+      nbValues = nbValues + 1
+    else
+      if value > max
+        max = value
       end
-      nb = nb+1      
+      if value < min
+        min = value
+      end
+      #uptime = nbvalues OK
+      mean = (mean*uptime + value) / (uptime+1)
+      uptime = uptime + 1
+      nbValues = nbValues + 1
+      archive.update(:max => max, :min => min, :mean => mean, :uptime => uptime, :nbValues => nbValues)
     end
-    if nb != 0
-      mean = mean / nb
-      uptime = uptime / nb
+  else #First Measure
+    max,min,mean,uptime,nbValues = 0,0,0,0,0
+    if value == -1 #KO
+      nbValues = 1
+    else #OK
+      max,min,mean,uptime,nbValues = value,value,value,1,1
     end
-    test.archives.create(:type => "day",:date => todayB,:max => max, :min => min, :mean => mean, :uptime => uptime, :nbValues => nb)
-  end    
-  return "Done"  
+    test.archives.create(:type => "day",:date => todayB,:max => max, :min => min, :mean => mean, :uptime => uptime, :nbValues => nbValues)  
+  end
+  
+  
 end
 
 get '/:id/list' do
@@ -153,15 +139,18 @@ end
 get '/go' do
   tests = Test.all
   tests.each do |test|
+    date, value = Time.new, 0
     begin
-      url = test.url      
-      before = Time.new
+      url = test.url            
       AppEngine::URLFetch.fetch(url, :method => 'GET')
       after = Time.new
-      test.results.create(:value => (after - before),:date => before)
+      value = after - date
+      test.results.create(:value => value,:date => date)
     rescue
-      test.results.create(:value => -1,:date => before)         
-    end 
+      value = -1
+      test.results.create(:value => value,:date => date)         
+    end
+    archivesDay(value,date,test)
   end
   return "Done"  
 end
